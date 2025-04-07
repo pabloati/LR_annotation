4f89d1a159155bcb3a494aca82fe6265d55c1544
pabloati <pabloatienzalo@gmail.com>
2025-04-07 09:53:37 +0200

    Modified lima renaming script

--- scripts/rename_lima_output.py
+++ scripts/rename_lima_output.py
@@ -1,7 +1,8 @@
 import os,sys
 
 def main():
-    dir=os.path.dirname(snakemake.input[0])
+    print("Input files: ", snakemake.input)
+    dir=os.path.dirname(os.path.dirname(snakemake.input[0]))
     samples_file = snakemake.params[0]
     brk_2_samples = {}
 
