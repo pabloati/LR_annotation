14046a0782b75224e909c0db02b9b6cf479cf1c3
pabloati <pabloatienzalo@gmail.com>
2025-08-06 10:59:36 +0200

    Local fixes

--- scripts/create_tama_filelist.py
+++ scripts/create_tama_filelist.py
@@ -2,9 +2,14 @@ import pandas as pd
 import sys
 
 files_dict = {}
-for file in sys.argv[1]:
-    sample = file.split("/")[-1].split("_filtered")[0]
+if sys.argv[1] is not list:
+    file = sys.argv[1]
+    sample= file.split("/")[-1].split("_filtered")[0]
     files_dict[sample] = file
+else:
+    for file in sys.argv[1]:
+        sample = file.split("/")[-1].split("_filtered")[0]
+        files_dict[sample] = file
 
 # Read TSV file
 tsv_df = pd.read_csv(sys.argv[2], sep="\t",index_col=None)
